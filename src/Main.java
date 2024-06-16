621c7eaa8fd934bde4e9664d7f6759552a8daa86
FeHunter <hunterfe1111@gmail.com>
2024-06-16 12:50:27 -0300

    Ajuste no menu de opções

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int op = Menu();
     }
 
-    static void Menu (){
+    static int Menu (){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Menu sistema de contas");
         System.out.println("1 - Incluir Conta");
-        System.out.println("1 - Alterar Saldo");
-        System.out.println("1 - Excluir Conta");
-        System.out.println("1 - Consultar Conta");
-        System.out.println("1 - Sair");
+        System.out.println("2 - Alterar Saldo");
+        System.out.println("3 - Excluir Conta");
+        System.out.println("4 - Consultar Conta");
+        System.out.println("5 - Sair");
+        int op = 0;
+        try {
+            op = sc.nextInt();
+        }catch (Exception e){
+            System.out.println("Você deve escolher uma das opções do menu");
+        }
+        return op;
     }
 }
